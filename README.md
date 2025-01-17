e4de4cfa78b940ac795ad5a76aeb30ae1cd1c450
KatiaBarba <katiabarba5@hotmail.com>
2025-01-17 17:49:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Desafios del Curso de Lógica de programación: sumérgete en la programación con JavaScript,
+de Alura Latam y Oracle Next Education.
---
+++ practica02.js
@@ -0,0 +1,58 @@
+/* CAMBIO
+DE PRUEBA*/
+
+/* 1. Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", 
+muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".*/
+
+let respuesta = prompt ("Que dia de la semana es? ");
+
+if (respuesta === "sabado"|| respuesta === "domingo");
+    {
+    alert ("¡Buen fin de semana!");
+    }
+else {
+    alert ("¡Buena semana!");
+    }
+
+
+/* 2. Verifica si un número ingresado por el usuario es positivo o negativo. 
+Muestra una alerta informativa.*/
+
+let numero = prompt ("Ingresa un numero: ");
+if (numero > 0);
+    {
+    alert ("El numero es positivo");
+    }
+else 
+    {
+    alert ("El numero es negativo");
+    }
+
+/* 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, 
+// muestra "¡Felicidades, has ganado!". 
+// En caso contrario, muestra "Intentalo nuevamente para ganar".*/
+
+let puntuacion = 110
+let score = puntuacion
+if (score >= 100);
+    {
+        alert ("¡Felicidades, has ganado!");
+    }
+else
+    {
+        alert ("Intentalo nuevamente para ganar");
+    }
+
+/* 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, 
+utilizando un template string para incluir el valor del saldo.*/
+
+let saldo = "5,000"
+alert (`Su saldo es: ${saldo}`);
+
+
+
+/*5. Pide al usuario que ingrese su nombre mediante un prompt. 
+Luego, muestra una alerta de bienvenida usando ese nombre.*/
+
+let nombre = prompt ("Ingresa tu nombre: ");
+alert (`Bienvenido, ${nombre}`);
