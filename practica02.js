0e4f28b03b4ccb02123d52bc50083dc18479b303
KatiaBarba <katiabarba5@hotmail.com>
2025-01-17 22:45:08 -0600

    correciones

--- practica02.js
+++ practica02.js
@@ -3,13 +3,14 @@ muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".*/
 
 let respuesta = prompt ("Que dia de la semana es? ");
 
-if (respuesta === "sabado"|| respuesta === "domingo");
-    {
+if (respuesta === "sabado" || respuesta === "domingo");
+{
     alert ("¡Buen fin de semana!");
-    }
-else {
+}
+else
+{
     alert ("¡Buena semana!");
-    }
+}
 
 
 /* 2. Verifica si un número ingresado por el usuario es positivo o negativo. 
@@ -17,13 +18,13 @@ Muestra una alerta informativa.*/
 
 let numero = prompt ("Ingresa un numero: ");
 if (numero > 0);
-    {
+{
     alert ("El numero es positivo");
-    }
-else 
-    {
+}
+else
+{
     alert ("El numero es negativo");
-    }
+}
 
 /* 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, 
 muestra "¡Felicidades, has ganado!". 
@@ -31,19 +32,19 @@ En caso contrario, muestra "Intentalo nuevamente para ganar".*/
 
 let puntuacion = 110;
 if (puntuacion >= 100);
-    {
-        alert ("¡Felicidades, has ganado!");
-    }
+{
+    console.log ("¡Felicidades, has ganado!");
+}
 else
-    {
-        alert ("Intentalo nuevamente para ganar");
-    }
+{
+    console.log ("Intentalo nuevamente para ganar");
+}
 
 /* 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, 
 utilizando un template string para incluir el valor del saldo.*/
 
-let saldo = "5,000"
-alert (`Su saldo es: ${saldo}`);
+let saldo = "5,000";
+alert (`El saldo de su cuenta es: ${saldo}`);
 
 
 /*5. Pide al usuario que ingrese su nombre mediante un prompt. 
