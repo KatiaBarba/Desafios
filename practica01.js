bbc48709b739cbd652666ee2f15608adcd06e2af
KatiaBarba <katiabarba5@hotmail.com>
2025-01-17 22:46:09 -0600

    correccion desafio 8, 9 y 10

--- practica01.js
+++ practica01.js
@@ -33,12 +33,17 @@ alert (mensajeDeError);
 
 
 // 8.- Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
-// 9.-Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
-/*10.- Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje 
-"¡Puedes obtener tu licencia de conducir!"*/
 
 let nombre = prompt ("Cual es tu nombre? ");
+
+
+// 9.-Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+
 let edad = prompt ("Ingresa tu edad: ");
+
+
+/*10.- Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje 
+"¡Puedes obtener tu licencia de conducir!"*/
 
 if (edad >= 18)
 {
