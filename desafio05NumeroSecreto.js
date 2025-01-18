2d954ce277bec5d58ac95a6912924fb291f5a417
KatiaBarba <katiabarba5@hotmail.com>
2025-01-18 11:47:52 -0600

    Desafio 05

---
+++ desafio05NumeroSecreto.js
@@ -0,0 +1,37 @@
+// Declaracion de variables:
+let numeroSecreto = Math.floor(Math.random()*100)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+let maximosIntentos = 3;
+
+while (numeroUsuario != numeroSecreto)
+{
+    numeroUsuario = parseInt(prompt("Adivina el número entre 1 y 10: "));
+
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) 
+    {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } 
+    else 
+    {
+        if (numeroUsuario > numeroSecreto) 
+        {
+            alert('El número secreto es menor');
+        } 
+        else 
+        {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos++;
+
+        if (intentos > maximosIntentos) 
+        {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+            alert('Lo siento, no acertaste el número');
+        }    
+    }
+}
