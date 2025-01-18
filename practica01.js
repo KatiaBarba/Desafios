af03f47f79072ac90da4af20f1e23932c02e4d51
KatiaBarba <katiabarba5@hotmail.com>
2025-01-17 21:32:45 -0600

    agrego primer desafio

--- practica01.js
+++ practica01.js
@@ -1,6 +1,8 @@
 // DESAFIOS DE LA CLASE 01 INICIANDO CON JAVASCRIPT
 
 //1.- Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+alert("¡Bienvenida y bienvenido a nuestro sitio web!"
+
 //2.- Declara una variable llamada nombre y asígnale el valor "Lua".
 //3. Crea una variable llamada edady asígnale el valor 25.
 //4.- Establece una variable numeroDeVentas y asígnale el valor 50.
