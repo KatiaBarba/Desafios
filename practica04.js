db31526c12a42bc91a0b6280c903f833187bdcb2
KatiaBarba <katiabarba5@hotmail.com>
2025-01-18 13:45:51 -0600

    actualizacion

--- practica04.js
+++ practica04.js
@@ -2,11 +2,11 @@
 
 // 1. Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
 //Opcion 1
-let nombre = prompt ("Cual es tu nombre?")
-console.log (Bienvenido, ${nombre}!)
+let nombre = prompt ("Cual es tu nombre?");
+console.log (Bienvenido, ${nombre}!);
 
 //Opcion 2
-console.log ("Bienvenido!")
+console.log ("Bienvenido!");
 
 /* 2. Crea una variable llamada "nombre" y asígnale tu nombre. Luego, 
 utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.*/
@@ -23,8 +23,8 @@ alert (`¡Hola, ${nombre}!`);
 ¿Cuál es el lenguaje de programación que más te gusta?. 
 Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.*/
 
-let respuesta = prompt (¿Cuál es el lenguaje de programación que más te gusta?)
-console.log(respuesta)
+let respuesta = prompt (¿Cuál es el lenguaje de programación que más te gusta?);
+console.log(respuesta);
 
 /* 5. Crea una variable llamada "valor1" y otra llamada "valor2", 
 asignándoles valores numéricos de tu elección. 
@@ -32,68 +32,92 @@ Luego, realiza la suma de estos dos valores y almacena el resultado
 en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje 
 "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.*/
 
-let valor1 = 5
-let valor2 = 10
-resultado = (valor1 + valor2)
-console.log (`La suma de ${valor1} y ${valor2} es igual a ${resultado}`)
+let valor1 = 5;
+let valor2 = 10;
+resultado = (valor1 + valor2);
+console.log (`La suma de ${valor1} y ${valor2} es igual a ${resultado}`);
 
 /* 6. Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos 
 de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado 
 en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje 
 "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.*/
 
-let valor1 = 83
-let valor2 = 15
-resultado = (valor1 - valor2)
-console.log (`La diferencia entre ${valor1} y ${valor2} es igual a ${resultado}`)
+let valor1 = 83;
+let valor2 = 15;
+resultado = (valor1 - valor2);
+console.log (`La diferencia entre ${valor1} y ${valor2} es igual a ${resultado}`);
 
 /* 7. Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, 
 utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje 
 apropiado en la consola.*/
 
-let edad = prompt ("Ingresa tu edad: ")
-if (edad >= 18)
+let edad = prompt ("Ingresa tu edad: ");
+if (edad >= 18);
 {
-    console.log('Eres mayor de edad')
+    console.log('Eres mayor de edad');
 }
 else 
 {
-    console.log('No eres mayor de edad')
+    console.log('No eres mayor de edad');
 }
 
 /* 8. Crea una variable "numero" y solicita un valor con prompt. 
 Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra 
 el mensaje correspondiente.*/
 
-let numero = prompt ('Ingresa un valor: ')
-if (numero = 0)
+let numero = prompt ('Ingresa un valor: ');
+if (numero = 0);
 {
-    alert ('El numero es cero')
+    alert ('El numero es cero');
 }
 else
 {
-    if (numero > 0)
+    if (numero > 0);
     {
-        alert ('El numero es positivo')
+        alert ('El numero es positivo');
     }
     else
-    { alert ('El numero es negativo')}
+    {
+        alert ('El numero es negativo');
+    }
 }
 
 // 9. Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
-
-
-
+let numero = 10;
+while (numero > 0);
+{
+    console.log (numero);
+    numero--;
+}
 
 /* 10. Crea una variable "nota" y asígnale un valor numérico. 
 Utiliza un if-else para determinar si la nota es mayor o igual a 7 
 y muestra "Aprobado" o "Reprobado" en la consola.*/
 
+let nota = 8;
+if (nota >= 7);
+{
+    console.log ("Aprobado");
+}
+else
+{
+    console.log ("Reprobado");
+}
+
 /* 11. Utiliza Math.random para generar cualquier número aleatorio 
 y muestra ese número en la consola.*/
+
+let numero = Math.floor(Math.random() * 1000 + 1);
+console.log = (numero)
 
 /* 12. Utiliza Math.random para generar un número entero entre 1 y 10 
 y muestra ese número en la consola.*/
 
+let numero = Math.floor(Math.random() * 10 + 1);
+console.log = (numero)
+
 /* 13. Utiliza Math.random para generar un número entero entre 1 y 1000 y 
 muestra ese número en la consola. */
+
+let numero = Math.floor(Math.random() * 1000 + 1);
+console.log = (numero)
